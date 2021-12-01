2d6d2a5137a3cfed78ecb36657c6efd2edab0e51
James <jamesle.mt@gmail.com>
2021-12-01 13:34:40 +0000

    day1 part b

--- day1/day1b.js
+++ day1/day1b.js
@@ -12,17 +12,20 @@ function solution (data) {
 	const theData = data.split('\n').map(Number);
 	let poopArray = [];
 	let peeArray = [];
-	console.log(theData);
+	// console.log(theData);
 	for (i = 0; i < theData.length; i++) {
 		const number = theData[i];
 		const nextNumber = theData[i+1];
-		if (nextNumber > number) {
-			poopArray.push(number + " " + nextNumber + ' poop');
-		} 
-		if (nextNumber < number) {
-			peeArray.push(number + " " + nextNumber + ' pee');
-		}
+		const nextNextNumber = theData[i+2];
+		poopArray.push(number + nextNumber + nextNextNumber);
 	}
 	console.log(poopArray);
+	for (i = 0; i < poopArray.length; i++) {
+		const number = poopArray[i];
+		const nextNumber = poopArray[i+1];
+		if (nextNumber > number) {
+			peeArray.push(number + " " + nextNumber + ' pee');
+		} 
+	}
 	console.log(peeArray);
 }
